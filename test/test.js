fa4e7be2a2bcc4aa614f2e58e911b7c71d1dae5f
permes <permes@unipost.co.kr>
2022-07-18 13:13:55 +0900

    modify

--- test/test.js
+++ test/test.js
@@ -1,11 +1,19 @@
-let arr = [];
+/* let arr = [];
 
 for(var i = 0; i < 5000000; ++i) {
     arr[i] = "ABC";
 }
 
 arr[arr.length] = "BBB";
-let bool = false;
+let bool = false; */
+
+/* var arr = ["a", "b", "c", "d"];
+var url = "";
+url = arr.reduce(function(a,b,c) {
+    return a + (c === 0 ? "?" : "&") + b + '=' + "AAA";
+}, url);
+
+console.log(url); */
 
 /* for(i = 0; i < arr.length; ++i) {
     if(new RegExp("^" + arr[i] + "$", 'gi').test("BBB")) {
@@ -24,6 +32,32 @@ let bool = false;
 /* bool = arr.reduce(function(a, b) {
     if(new RegExp("^" + b + "$", 'gi').test("BBB")) a = true;
     return a;
-}, false); */
+}, false); 
 
 console.log(bool);
+*/
+
+let obj = {};
+
+for(let i = 1; i <= 5000000; ++i ) {
+    obj["a"+i] = "test";
+}
+
+function isEmptyObject(o) {
+    return getKeys(o).length === 0;
+}
+
+function getKeys(o) {
+    return Object.keys(o);
+}
+
+/* function isEmptyObject(o) {
+    var b = true;
+    for(let k in o) {
+        b = false;
+        break;
+    }
+    return b;
+} */
+
+console.log(isEmptyObject(obj));
