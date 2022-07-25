ce8954afb3b1e0a3c9badfe061d784f8445767d0
permes <permes@unipost.co.kr>
2022-07-25 13:10:06 +0900

    test

--- test/test.ts
+++ test/test.ts
@@ -1,3 +1,19 @@
-function aaa() {
-    
+/* function identity<T>(arg: T): T {
+    console.log(arg.length);
+    return arg;
+} */
+
+
+/* function identity<T>(arg: Array<T>): Array<T> {
+    console.log(arg.length);
+    return arg;
+} */
+
+//identity(["1"]);
+
+// 제네릭 타입
+function identity<T>(arg: T): T {
+    return arg;
 }
+
+let myIdentity: <T>(arg: T) => T = identity
