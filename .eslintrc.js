95ff930e9e5b86184141518247311a53f10eac70
nermer1 <nermer1@gmail.com>
2022-08-07 19:26:49 +0900

    merge

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,29 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
+  "env": {
+      "browser": true,
+      "es2021": true,
+      "es6": true,
+      "node": true
   },
-  extends: [
-    'airbnb-base',
+  "extends": [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:@typescript-eslint/recommended"
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+      "ecmaFeatures": {
+          "jsx": true
+      },
+      "ecmaVersion": "latest",
+      "sourceType": "module"
   },
-  plugins: [
-    '@typescript-eslint',
+  "plugins": [
+      "react",
+      "@typescript-eslint"
   ],
-  rules: {
-  },
-};
+  "rules": {
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"]
+  }
+}
---
+++ test/lunarData.ts
@@ -0,0 +1,8 @@
+type Test = {
+    [key: string]: string
+};
+const data: Test = {
+    "test": "test"
+}; 
+
+//console.log(data);
