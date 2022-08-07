8858f88a0aa664ca985b990a5ccefeace18091a9
permes <permes@unipost.co.kr>
2022-08-07 19:14:05 +0900

    add test

--- test/test.js
+++ test/test.js
@@ -37,7 +37,7 @@ console.log(url); */
 console.log(bool);
 */
 
-let obj = {};
+/* let obj = {};
 
 for(let i = 1; i <= 5000000; ++i ) {
     obj["a"+i] = "test";
@@ -49,7 +49,7 @@ function isEmptyObject(o) {
 
 function getKeys(o) {
     return Object.keys(o);
-}
+} */
 
 /* function isEmptyObject(o) {
     var b = true;
@@ -60,4 +60,73 @@ function getKeys(o) {
     return b;
 } */
 
-console.log(isEmptyObject(obj));
+//console.log(isEmptyObject(obj));
+
+//class test
+/* class Geoposition {
+    constructor(coords, timestamp) {
+        this.coords = coords;
+        this.timestamp = timestamp;
+    }
+}; */
+
+/* class Postion extends Geoposition {
+    constructor() {
+        super(new Coordinates2(), new Date().getTime());
+    }
+} */
+
+const GeolocationPosition = function() {
+    this.coords = new Coordinates2();
+    this.timestamp = new Date().getTime();
+}
+
+/* GeolocationPosition.prototype = {
+    coords: new Coordinates2(),
+    timestamp: new Date().getTime()
+} */
+
+function Coordinates2() {
+    this.acuuracy = 1;
+	this.altitude = 2;
+}
+
+class Navigator2 {
+	geolocation = {
+		getCurrentPosition: function(callback) {
+			callback(new GeolocationPosition());
+		}
+	}
+};
+
+/* function Geoposition() {
+    this.coords = new Coordinates2();
+    this.timestamp = new Date().getTime();
+} */
+
+
+const n = new Navigator2();
+n.geolocation.getCurrentPosition((obj) => console.log(obj));
+
+/* class Polygon {
+    constructor(height, width) {
+      this.height = height;
+      this.width = width;
+    }
+  }
+  
+  class Square extends Polygon {
+    constructor(sideLength) {
+      super(sideLength, sideLength);
+    }
+    get area() {
+      return this.height * this.width;
+    }
+    set sideLength(newLength) {
+      this.height = newLength;
+      this.width = newLength;
+    }
+  }
+  
+  var square = new Square(2);
+  console.log(square); */
--- test/test.ts
+++ test/test.ts
@@ -12,8 +12,94 @@
 //identity(["1"]);
 
 // 제네릭 타입
-function identity<T>(arg: T): T {
+/* function identity<T>(arg: T): T {
     return arg;
 }
 
-let myIdentity: <T>(arg: T) => T = identity
+let myIdentity: <T>(arg: T) => T = identity */
+
+type Bus = {
+    color : string,
+    model : boolean,
+    price : number
+}
+
+type TypeChanger <T> = {
+    [key in keyof T]: string | number;
+};
+
+type newType = TypeChanger<Bus>;
+
+let obj: newType = {
+    color: 'red',
+    model : 'kia',
+    price :300
+}
+
+
+/* type Bus = {
+    color : string,
+    model : boolean,
+    price : number
+  }
+  
+  type TypeChanger <MyType, T> = {
+    [key in keyof MyType]: T;
+  };
+  
+  type NewBus = TypeChanger<Bus, boolean>;
+  type NewBus2 = TypeChanger<Bus, string[]> */
+
+  /* interface Type {
+    [key: string]: number | Type
+  }
+
+  let a: Type = {
+    'font-size' : {
+      'font-size' : {
+        'font-size' : 14
+      }
+    }
+  } */
+
+  type Type1 = {
+    [key: string]: string|number
+  };
+
+  let obj2: Type1 = {
+    model : 'k5',
+    brand : 'kia',
+    price : 6000,
+    year : 2030,
+    date : '6월',
+    percent : '5%',
+    dealer : '김차장',
+  }
+
+  interface Type2 {
+    [key: string]: Type2|number
+  }
+
+  let obj3: Type2 = {
+    'font-size' : 10,
+    'secondary' : {
+      'font-size' : 12,
+      'third' : {
+        'font-size' : 14
+      }
+    }
+  }
+
+
+  interface CarType {
+    model: string,
+    price: number
+  }
+
+  class Car implements CarType{
+    model: string;
+    price: number = 1000;
+    constructor(a: string) {
+        this.model = a;
+    }
+  }
